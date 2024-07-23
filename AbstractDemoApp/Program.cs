88d1cc92fe06e92120fd112614871ddb794a3a89
deoresulakshana <deoresulakshana@gmail.com>
2024-07-23 12:58:30 +0530

    Add project files.

---
+++ AbstractDemoApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using AbstractDemoLibrary;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ElectronicProducts ep=new ElectronicProducts();
+        ep.ProdID = 1;
+        ep.ProductName = "Test";
+        ep.Price= 100;
+        ep.MfgDate = new DateOnly(2024, 7, 22);
+        ep.PrintProductDescription();
+        Console.WriteLine(ep.Description);
+
+        GlassProducts gp=new GlassProducts();
+        gp.ProdID = 101;
+        gp.ProductName = "Glass1";
+        gp.Price = 100;
+        gp.PrintProductDescription();
+        Console.WriteLine(gp.Note);
+        Console.Read();
+
+    }
+}
---
+++ AbstractDemoLibrary/Class1.cs
@@ -0,0 +1,45 @@
+﻿namespace AbstractDemoLibrary
+{
+    public abstract class Products
+    {
+        //concrete member/s--- have body {}
+        public int ProdID { get; set; }
+        public string ProductName { get; set; }
+        public int Price { get; set; }
+        
+        //abstract member---has no body {}
+        //abstract members are virtual by default
+        public abstract void PrintProductDescription();
+
+        public DateOnly MfgDate { get; set; }
+    }
+
+
+    public class ElectronicProducts : Products
+    {
+        
+        public string Description { get;  private set; }
+        public override void PrintProductDescription()
+        {
+
+            Description = $"Electronic Product, has  a mfg date {base.MfgDate}";
+
+           // throw new NotImplementedException();
+
+        }
+
+    }
+
+    public class GlassProducts : Products
+    {
+        public string Note { get; set; }
+
+        public override void PrintProductDescription()
+        {
+            Note = "As this is a glass product,no guarantee/warranty";
+        //    throw new NotImplementedException();
+        }
+
+    }
+
+}
